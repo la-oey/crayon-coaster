a487b12e50737ceb39ce33ef098d88853c81ae79
Lauren Oey <loey@princeton.edu>
2024-04-17 20:59:57 -0400

    run simulations locally

--- physics_sim_model/run.py
+++ physics_sim_model/run.py
@@ -7,9 +7,10 @@ import subprocess
 file = open('solutions.json')
 solutions = json.load(file)
 
-n = 5
+# initiate local server
+subprocess.Popen(["php", "-S", "localhost:8000"])
 
-for sid in list(solutions[sys.argv[1]+"-"+sys.argv[2]].keys())[:n]:
+for sid in solutions[sys.argv[1]+"-"+sys.argv[2]]:
 	# print solutions being opened
 	print(sid) 
 
@@ -17,7 +18,10 @@ for sid in list(solutions[sys.argv[1]+"-"+sys.argv[2]].keys())[:n]:
 	level = "level=" + sys.argv[1]
 	wind = "&wind=" + sys.argv[2]
 	imageid = "&imageid=" + sid[:-4]
-	url = "https://lo.velezlab.opalstacked.com/physics_sim_model/index.html?"+level+wind+imageid
+	# local url
+	url = "http://localhost:8000/?"+level+wind+imageid
+	# web url
+	# url = "https://lo.velezlab.opalstacked.com/physics_sim_model/index.html?"+level+wind+imageid
 
 	browser = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" #google chrome in MacOS
 	subprocess.Popen([browser, "--new-window", url]) #opens simulation in new window
